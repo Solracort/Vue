518b27782aef088154f87f310ceaeef6483181b5
cortiz-s <cortiz-s@MacBook-Air-de-cortiz-s.local>
2023-09-21 12:18:32 +0200

    feat(75): first test included

--- 03-fundamentos/tests/unit/components/counter.spec.js
+++ 03-fundamentos/tests/unit/components/counter.spec.js
@@ -1,9 +1,16 @@
 
 import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
+import { TestWatcher, createTestScheduler } from 'jest'
 
 
 describe ('Counter Component', ()=>{
+
+    let wrapper 
+
+    beforeEach(()=> {
+        wrapper = shallowMount(Counter)
+    })
 
     // test('Debe hacer match con el snapshot', () => { 
 
@@ -14,37 +21,48 @@ describe ('Counter Component', ()=>{
 
     test('h2 debe tener el valor por defecto "Counter"', () => 
    { 
-        const wrapper = shallowMount(Counter)
 
         const h2Value = wrapper.find('h2').text()
 
         expect (h2Value).toBe('Counter')
     })
-    test('El valor por defecto en la etiqueta <p> debe ser 100', () => { 
+
+    // test('El valor por defecto en la etiqueta <p> debe ser 100', () => { 
         // Wrapper 
-        const wrapper = shallowMount(Counter);
         // pTags
         // const pTags = wrapper.findAll('p')
-        const value = wrapper.find('[data-testid="counter"]')
+        // const value = wrapper.find('[data-testid="counter"]')
 
         // expect segundo p=== 100
         // expect(pTags[0].text()).toBe('100')
-        expect((value).text()).toBe('100');
-    })
+    //     expect((value).text()).toBe('100');
+    // })
+
     test('Debe incrementar y decrementar el contador', async() => { 
-        const wrapper = shallowMount (Counter);
 
-        const increaseBtn = wrapper.find('button')  //encuentra el primero y no busca más coincidencias
-        await  increaseBtn.trigger('click');
-        let value = wrapper.find('[data-testid="counter"]').text()
-        expect(value).toBe('101')
-
-        const decreaseBtn = wrapper.findAll('button')[1]  //encuentra el primero y no busca más coincidencias;
-
+        const [increaseBtn, decreaseBtn] = wrapper.findAll('button')
+        
+        await increaseBtn.trigger('click');
+        await increaseBtn.trigger('click');
+        await increaseBtn.trigger('click');
+        
+        
         await decreaseBtn.trigger('click');
         await decreaseBtn.trigger('click');
-        value = wrapper.find('[data-testid="counter"]').text()
 
-        expect(value).toBe('99');    
+        const value = wrapper.find('[data-testid="counter"]').text()
+       
+        expect(value).toBe('101');   
+           
     })
+    test('Debe establecer el valor por defecto ', () => {
+
+        // const start = wrapper.props('start')
+        // otra forma
+        const {start} = wrapper.props();
+        // console.log ( start );
+        const value = wrapper.find('[data-testid="counter"]').text();
+        expect (Number(value)).toBe( start );
+    });
+
 })
